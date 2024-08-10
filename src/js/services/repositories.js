8dfb026a2ada9c263e11a6319cd6c7ea8c5a068e
Miguel <pokassbp@gmail.com>
2024-08-09 23:49:10 -0300

    Update repositories.js

--- src/js/services/repositories.js
+++ src/js/services/repositories.js
@@ -1,5 +1,5 @@
 async function buscarRepositoriosDoUsuario(users) {
-    const resposta = await (await fetch(`https://api.github.com/users/${users}/repos`)).json()
+    const resposta = await (await fetch(`https://api.github.com/users/${users}/repos?per_page=10`)).json()
     return await resposta
 }
 
